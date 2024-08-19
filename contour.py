28208365745ea444a471864f2f8bef89b7255f47
paarthminear343 <paarthminear343@gmail.com>
2024-08-20 04:58:33 +0530

    updated

--- contour.py
+++ contour.py
@@ -3,4 +3,7 @@ import cv2 as cv
 
 img = cv. imread( '/Users/paarthminear/Downloads/PM/Code/VS code/image processing/Photos/group 1.jpg')
 cv. imshow( 'Group photo', img)
+
+# gray = 
+
 cv. waitKey (0)
